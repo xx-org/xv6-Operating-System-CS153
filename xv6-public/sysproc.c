4a7d35c3ba23e70ea1676c1543acfb84fa82a48c
Xichao Wang <xwang296@sledge.cs.ucr.edu>
2019-04-29 19:10:17 -0700

    upload ini

---
+++ xv6-public/sysproc.c
@@ -0,0 +1,125 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  int* status;
+  argptr(0,(void*)& status, sizeof(status));
+  return wait(status);
+} //cs153 lab1
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+// BR
+int
+sys_hello(void)
+{
+  hello();
+  return 0;
+}
+// BR
+
+int
+sys_exit_lab(void)
+{
+  int exit_status;
+  if(argint(0,&exit_status)<0)
+	return -1;
+  exit_lab(exit_status);
+  return 0; //not reached
+}
+//cs153 lab1
+int
+sys_waitpid(void)
+{
+  int pid, options;
+  int* status;
+  if(argint(0, &pid) < 0)
+    return -1;
+  argptr(1, (void*) &status, sizeof(status));
+  if(argint(2, &options) < 0)
+    return -1;
+  return waitpid(pid, status, options);
+}//cs153 lab1
